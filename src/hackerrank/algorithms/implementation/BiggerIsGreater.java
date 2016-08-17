36ba64d634e25d58ef6845f274bdf2317a5f25df
DeepakVadgama <vadgama.deepak@gmail.com>
2016-08-17 23:02:52 +0530

    https://www.hackerrank.com/challenges/bigger-is-greater/ -- Finally working

--- src/hackerrank/algorithms/implementation/BiggerIsGreater.java
+++ src/hackerrank/algorithms/implementation/BiggerIsGreater.java
@@ -1,5 +1,6 @@
 package hackerrank.algorithms.implementation;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,38 +13,69 @@ import java.util.stream.Collectors;
  */
 public class BiggerIsGreater {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+//        List<String> input = Files.readAllLines(Paths.get("/home/deepak/Desktop/input01.txt"));
+//        List<String> output = Files.readAllLines(Paths.get("/home/deepak/Desktop/output01.txt"));
+//
+//        for (int i = 0; i < input.size(); i++) {
+//            final String outputVal = solveThis(input.get(i));
+//            if (!outputVal.equals(output.get(i))) {
+//                System.out.println("Problem");
+//                System.out.println("Input: " + input.get(i));
+//                System.out.println("Actual   Output: " + outputVal);
+//                System.out.println("Expected Output: " + output.get(i));
+//                solveThis(input.get(i));
+//                System.exit(0);
+//            } 
+//        }
+
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        List<List<Character>> strings = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
             final String string = in.next();
             final List<Character> list = string.chars()
                     .mapToObj(e -> (char) e)
                     .collect(Collectors.toList());
-            strings.add(list);
+            System.out.println(solveThis(list));
         }
+    }
 
-        for (List<Character> list : strings) {
-            if (list.size() == 1 || isDescending(list)) {
-                System.out.println("no answer");
-            } else {
-                int pivotIndex = getPivot(list);
-                char pivot = list.get(pivotIndex);
-                List<Character> firstPart = new ArrayList<>();
-                if (pivotIndex > 0) {
-                    firstPart = new ArrayList<>(list.subList(0, pivotIndex));
-                }
-                List<Character> secondPart = new ArrayList<>(list.subList(pivotIndex, list.size()));
-                Collections.sort(secondPart);
-                final int index = secondPart.indexOf(pivot);
-                char newPivot = secondPart.get(index + 1);
-                secondPart.remove(index + 1);
-                Collections.sort(secondPart);
-                System.out.println(getString(firstPart) + newPivot + getString(secondPart));
+    private static String solveThis(String string) {
+        final List<Character> list = string.chars()
+                .mapToObj(e -> (char) e)
+                .collect(Collectors.toList());
+        return solveThis(list);
+    }
+
+    private static String solveThis(List<Character> list) {
+        String result = "no answer";
+        if (list.size() != 1 && !isDescending(list)) {
+            int pivotIndex = getPivot(list);
+            char pivot = list.get(pivotIndex);
+            List<Character> l1 = new ArrayList<>();
+            if (pivotIndex > 0) {
+                l1 = new ArrayList<>(list.subList(0, pivotIndex));
+            }
+            List<Character> l2 = new ArrayList<>(list.subList(pivotIndex, list.size()));
+            Collections.sort(l2);
+            int newPivotIndex = newPivot(l2, pivot);
+            char newPivot = l2.get(newPivotIndex);
+            l2.remove(newPivotIndex);
+            Collections.sort(l2);
+            result = getString(l1) + newPivot + getString(l2);
+        }
+        return result;
+    }
+
+    private static int newPivot(List<Character> list, Character pivot) {
+        for (Character character : list) {
+            if (character > pivot) {
+                return list.indexOf(character);
             }
         }
+        return -1;
     }
 
     private static String getString(List<Character> chars) {
