27b15fd80f1ee271e8d35efac0f2fbcce2c35d59
Deepak Vadgama <vadgama.deepak@gmail.com>
2016-09-17 15:45:15 +0530

    https://www.hackerrank.com/challenges/library-fine

---
+++ src/hackerrank/algorithms/implementation/LibraryFine.java
@@ -0,0 +1,34 @@
+package hackerrank.algorithms.implementation;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/angry-professor
+ */
+public class LibraryFine {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        LocalDate act = getDate(in);
+        LocalDate exp = getDate(in);
+
+        if (act.isBefore(exp) || act.isEqual(exp)) {
+            System.out.println(0);
+        } else if (act.getYear() != exp.getYear()) {
+            System.out.println("10000");
+        } else if (act.getMonthValue() != exp.getMonthValue()) {
+            System.out.println((act.getMonthValue() - exp.getMonthValue()) * 500);
+        } else {
+            System.out.println((act.getDayOfMonth() - exp.getDayOfMonth()) * 15);
+        }
+    }
+
+    public static LocalDate getDate(Scanner in) {
+        int d = in.nextInt();
+        int m = in.nextInt();
+        int y = in.nextInt();
+        return LocalDate.of(y, m, d);
+    }
+}
