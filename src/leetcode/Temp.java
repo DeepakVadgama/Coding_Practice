bd3ad3b73a8a6e50f627746e2b870f4e90a41db9
Deepak Vadgama <vadgama.deepak@gmail.com>
2016-10-31 14:52:10 +0530

    https://leetcode.com/problems/longest-absolute-file-path/
    Working perfectly fine on local, but stupid leetcode thinks otherwise

---
+++ src/leetcode/Temp.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+public class Temp {
+    public static void main(String[] args) {
+        String subpath = "\\t\\tfileext.text";
+        System.out.println(subpath.replaceAll("\\\\t", ""));
+    }
+}
