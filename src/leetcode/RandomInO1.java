beb98464e1c46120d969dece20f37559b4f405c7
Deepak Vadgama <vadgama.deepak@gmail.com>
2016-11-21 11:16:57 +0530

    https://leetcode.com/problems/insert-delete-getrandom-o1/  - Fixed O(1) removal

--- src/leetcode/RandomInO1.java
+++ src/leetcode/RandomInO1.java
@@ -1,29 +1,27 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
  * https://leetcode.com/problems/insert-delete-getrandom-o1/
  */
 public class RandomInO1 {
-    public static void main(String[] args) {
-
-    }
 
     public class RandomizedSet {
 
+        private Map<Integer, Integer> indexMap;
         private ArrayList<Integer> values;
-        private HashSet<Integer> valueSet;
         private Random random;
 
         /**
          * Initialize your data structure here.
          */
         public RandomizedSet() {
+            indexMap = new HashMap<>();
             values = new ArrayList<>();
-            valueSet = new HashSet<>();
             random = new Random();
         }
 
@@ -31,9 +29,9 @@ public class RandomInO1 {
          * Inserts a value to the set. Returns true if the set did not already contain the specified element.
          */
         public boolean insert(int val) {
-            if (!valueSet.contains(val)) {
-                valueSet.add(val);
+            if (!indexMap.containsKey(val)) {
                 values.add(val);
+                indexMap.put(val, values.size() - 1);
                 return true;
             }
             return false;
@@ -43,9 +41,18 @@ public class RandomInO1 {
          * Removes a value from the set. Returns true if the set contained the specified element.
          */
         public boolean remove(int val) {
-            if (valueSet.contains(val)) {
-                valueSet.remove(val);
-                values.remove(Integer.valueOf(val));
+            if (indexMap.containsKey(val)) {
+
+                int index = indexMap.get(val);  // get location
+
+                if (index != values.size() - 1) {
+                    values.set(index, values.get(values.size() - 1)); // replace indexed position with last element
+                    indexMap.put(values.get(index), index);  // update position of element in index map
+                }
+
+                values.remove(values.size() - 1); // remove last element
+                indexMap.remove(val);  // remove from index map
+
                 return true;
             }
             return false;
