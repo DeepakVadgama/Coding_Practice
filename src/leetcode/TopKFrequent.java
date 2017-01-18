1cc37c85907612f3523e5f567a013a833d4bdf41
Deepak Vadgama <vadgama.deepak@gmail.com>
2017-01-18 10:34:39 +0530

    https://leetcode.com/problems/top-k-frequent-elements/

---
+++ src/leetcode/TopKFrequent.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/top-k-frequent-elements/
+ */
+public class TopKFrequent {
+
+    public static void main(String[] args) {
+        System.out.println(topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2));
+    }
+
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        }
+
+        // frequency can be max nums.length
+        // wastes lot of space (many slots will be blank),
+        // but this avoid having to sort the map by value
+        List<Integer>[] buckets = new List[nums.length];
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (buckets[entry.getValue()] == null) {
+                buckets[entry.getValue()] = new ArrayList<>();
+            }
+            buckets[entry.getValue()].add(entry.getKey());
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0 && result.size() < k; i--) {
+            if (buckets[i] != null) {
+                result.addAll(buckets[i]);
+            }
+        }
+        return result.subList(0, k); // In case buckets[pos] has more elements than k
+    }
+}
