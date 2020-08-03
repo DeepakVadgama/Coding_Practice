11e6a519dd598e188711a846b827f4149184c2f7
Deepak Vadgama <vadgama.deepak@gmail.com>
2020-08-03 17:54:33 +0530

    Create LCM_BT_236.py

---
+++ src/main/java/leetcode/LCM_BT_236.py
@@ -0,0 +1,16 @@
+def lowestCommonAncestor(self, root, p, q):
+				# If looking for me, return myself
+        if root == p or root == q:
+            return root
+        
+        left = right = None
+        # else look in left and right child
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        else:
+            return left or right
